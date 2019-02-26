0a0746ecdd3740b0f8a117ad3ff57fe70502fb63
Adam Dunajewski <adam.dunajewski@cybercom.com>
2019-02-26 12:17:43 +0100

    Section 6 OOP1 Classes: Wall, UberClass, Main

---
+++ Section6_OOP1/src/pl/adulek/Main.java
@@ -0,0 +1,19 @@
+package pl.adulek;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UberClass uberClass = new UberClass();
+
+        System.out.println(uberClass.bool);
+        System.out.println(uberClass.byt);
+        System.out.println(uberClass.shor);
+        System.out.println(uberClass.in);
+        System.out.println(uberClass.lon);
+        System.out.println(uberClass.flo);
+        System.out.println(uberClass.doub);
+        System.out.println(uberClass.strin);
+        System.out.println(uberClass.cha);
+
+    }
+}
---
+++ Section6_OOP1/src/pl/adulek/UberClass.java
@@ -0,0 +1,14 @@
+package pl.adulek;
+
+public class UberClass {
+    public boolean bool;
+    public byte byt;
+    public short shor;
+    public int in;
+    public long lon;
+    public float flo;
+    public double doub;
+    public String strin;
+    char cha;
+
+}
