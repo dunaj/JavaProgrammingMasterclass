da39426e385b601344f98f1357cd8981f72771fa
dunaj <adam.doonay@gmail.com>
2019-02-27 19:49:04 +0100

    Finished the Section 6 with two Inheritance tasks.

---
+++ Section6_OOP1/src/pl/adulek/Circle.java
@@ -0,0 +1,19 @@
+package pl.adulek;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+        if (this.radius < 0.0)
+            this.radius= 0.0;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public  double getArea() {
+        return radius*radius*Math.PI;
+    }
+}
